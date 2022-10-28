7e68d11b19811729d8470bd4b4815989792f434e
daehwan0307 <eoghks960307@naver.com>
2022-10-28 13:43:46 +0900

    Add UserController

--- src/main/java/com/springboot/hello/configuration/SwaggerConfig.java
+++ src/main/java/com/springboot/hello/configuration/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.springboot.hello.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,6 +18,14 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0")
                 .build();
     }
 }
--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -1,4 +1,18 @@
 package com.springboot.hello.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserDao {
+    private  final JdbcTemplate jdbcTemplate;
+
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int deleteAll(){
+        return this.jdbcTemplate.update("delete  from users");
+    }
 }
