efd670dc893a8900c54162486a814737264b62ea
daehwan0307 <eoghks960307@naver.com>
2022-11-02 13:18:22 +0900

    feat) putController 수정

--- src/main/java/com/springboot/hello/controller/HospitalController.java
+++ src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -1,16 +1,35 @@
 package com.springboot.hello.controller;
 
 
+import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/hospital")
+@RequestMapping(value = "/api/hospital",method = RequestMethod.GET)
 public class HospitalController {
 
-    @RequestMapping("/request-dto")
-    public String getNameDto(Hospital hospital){
-        return hospital.getHospitalName();
+
+    private final HospitalDao hospitalDao;
+
+    public HospitalController(HospitalDao hospitalDao) {
+        this.hospitalDao = hospitalDao;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hospital> get(@PathVariable Integer id) {
+        Hospital hospital = hospitalDao.findById(id);
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        if (!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
+    }
+
 }
--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -3,15 +3,30 @@ package com.springboot.hello.controller;
 import com.springboot.hello.domain.dto.MemberDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/put-api")
 public class PutController {
-    @PostMapping("/member3")
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample(){
+        return "Hello Post Api";
+    }
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData){
+        StringBuilder sb = new StringBuilder();  //Builder Pattern
+
+        postData.entrySet().forEach(map->sb.append
+                (map.getKey()+":"+map.getValue()+"\n"));
+        return sb.toString();
+    }
+    @PostMapping("/member2")
+    public String postMember(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
+    }
+    @PutMapping("/member3")
     public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
