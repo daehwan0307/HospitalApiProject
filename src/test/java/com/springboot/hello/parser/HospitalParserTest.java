8f863ba23158ea78e629f53c4f7f2de0a24e8464
daehwan0307 <eoghks960307@naver.com>
2022-11-02 14:05:17 +0900

    feat) insert hospital data

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -2,6 +2,7 @@ package com.springboot.hello.parser;
 
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
+import com.springboot.hello.service.HospitalService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
     String line770 = "\"769\",\"의원\",\"01_01_02_P\",\"5710000\",\"PHMA119964360069041100011\",\"19960328\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"298-8007\",\"\",\"360812\",\"충청북도 청주시 상당구 용암동 2627번지\",\"충청북도 청주시 상당구 중고개로 181, 2층 (용암동)\",\"28763\",\"상당이비인후과의원\",\"20170905183708\",\"I\",\"2018-08-31 23:59:59.0\",\"의원\",\"245973.624694\",\"346712.43852\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",";
+
+    @Autowired
+    HospitalService hospitalService;
 
     @Autowired // spring
     ReadLineContext<Hospital> hospitalReadLineContext;
@@ -62,15 +66,14 @@ class HospitalParserTest {
     void oneHundreadThousandRows() throws IOException {
         // 서버환경에서 build할 때 문제가 생길 수 있습니다.
         // 어디에서든지 실행할 수 있게 짜는 것이 목표.
+        hospitalDao.deleteAll();
         String filename = "C:\\Users\\daehwan\\Desktop\\fulldata_01_01_02_P_의원.csv";
-        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
-        assertTrue(hospitalList.size() > 1000);
-        assertTrue(hospitalList.size() > 10000);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(hospitalList.get(i).getHospitalName());
-        }
-        System.out.printf("파싱된 데이터 개수:%d", hospitalList.size());
+        int cnt = this.hospitalService.insertLargeVolumeHospitalData(filename);
+        assertTrue(cnt > 1000);
+        assertTrue(cnt > 10000);
+        System.out.printf("파싱된 데이터 개수:%d", cnt);
     }
+
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는지 Test")
